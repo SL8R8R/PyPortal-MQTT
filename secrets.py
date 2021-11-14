f6f784788513f77d4ed34f5b1ae04ca96ac7c0f4
SL8R8R <54902014+SL8R8R@users.noreply.github.com>
2021-11-14 14:40:23 -0800

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,12 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'ssid',
+    'password' : 'password',
+    'timezone' : "America/Los_Angeles", # http://worldtimeapi.org/timezones
+    'broker' : 'mqtt broker',
+    'user' : 'mqtt user',
+    'pass' : 'mqtt password',
+    'port' : 1883
+    }
